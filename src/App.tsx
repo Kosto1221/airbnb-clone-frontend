0cd8efb45eac76b4aef8ab938ccc84a3b9cdfaf3
Kosto1221 <ostolany@gamil.com>
2024-12-24 16:20:44 +0900

    FRONT-END SETUP_Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Text color={"red.200"} fontSize={"6xl"}>
+        It works!!!
+      </Text>
+    </>
+  );
+}
+
+export default App;
