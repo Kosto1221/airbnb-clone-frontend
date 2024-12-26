924b90d8dcb66d962babea40262364ca5538735e
Kosto1221 <ostolany@gamil.com>
2024-12-26 21:37:10 +0900

    Axios

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,30 @@
-const BASE_URL = "http://127.0.0.1:8000/api/v1";
+import axios from "axios";
 
-export async function getRooms() {
-  const response = await fetch(`${BASE_URL}/rooms/`);
-  const json = await response.json();
-  return json;
-}
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1/",
+});
+
+// const axiosInstance = axios.create({
+//   baseURL: "http://127.0.0.1:8000/api/v1/",
+// });
+
+// const BASE_URL = "http://127.0.0.1:8000/api/v1";
+
+// export async function getRooms() {
+//   const response = await fetch(`${BASE_URL}/rooms/`);
+//   const json = await response.json();
+//   return json;
+// }
+
+// export async function getRooms() {
+//   const response = await axios.get(`${BASE_URL}/rooms/`);
+//   return response.data;
+// }
+
+export const getRooms = () =>
+  instance.get("rooms/").then((response) => response.data);
+
+// export async function getRooms() {
+//   const response = await axiosInstance.get("room/");
+//   return response.data;
+// }
