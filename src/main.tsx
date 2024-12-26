40707999167e1f0bb82f13f14142ea4546c34397
Kosto1221 <ostolany@gamil.com>
2024-12-26 21:25:35 +0900

    React Query

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <QueryClientProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
