9a2b25bca23768faf9e012e9df9de4ff2dc8d247
Kosto1221 <ostolany@gamil.com>
2024-12-29 22:05:29 +0900

    Reviews

--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
-import { getRoom } from "../api";
+import { getRoom, getRoomReviews } from "../api";
 import { useQuery } from "@tanstack/react-query";
-import { IRoomDetail } from "../types";
+import { IReview, IRoomDetail } from "../types";
 import {
   Box,
   Grid,
@@ -9,7 +9,13 @@ import {
   Skeleton,
   Image,
   GridItem,
+  HStack,
+  VStack,
+  Text,
+  Avatar,
+  Container,
 } from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
 
 export default function RoomDetail() {
   const { roomPk } = useParams();
@@ -17,8 +23,11 @@ export default function RoomDetail() {
     queryKey: [`rooms`, roomPk],
     queryFn: getRoom,
   });
+  const { data: reviewsData, isLoading: isReviewsLoading } = useQuery<
+    IReview[]
+  >({ queryKey: [`rooms`, roomPk, `reviews`], queryFn: getRoomReviews });
   return (
-    <Box mt={10} px={{ base: 10, lg: 40 }}>
+    <Box pb={40} mt={10} px={{ base: 10, lg: 40 }}>
       <Skeleton height={"43px"} width="25%" isLoaded={!isLoading}>
         <Heading>{data?.name}</Heading>
       </Skeleton>
@@ -26,7 +35,7 @@ export default function RoomDetail() {
         mt={8}
         rounded="xl"
         overflow={"hidden"}
-        gap={3}
+        gap={2}
         templateRows={"1fr 1fr"}
         templateColumns={"repeat(4, 1fr)"}
         height="60vh"
@@ -38,7 +47,7 @@ export default function RoomDetail() {
             overflow={"hidden"}
             key={index}
           >
-            <Skeleton isLoaded={!isLoading} h="100%">
+            <Skeleton isLoaded={!isLoading} h="100%" w="100%">
               {data?.photos[index] ? (
                 <Image
                   w="100%"
@@ -51,6 +60,63 @@ export default function RoomDetail() {
           </GridItem>
         ))}
       </Grid>
+      <HStack width={"40%"} justifyContent={"space-between"} mt={10}>
+        <VStack alignItems={"flex-start"}>
+          <Skeleton isLoaded={!isLoading} height={"30px"}>
+            <Heading fontSize={"2xl"}>
+              House hosted by {data?.owner.name}
+            </Heading>
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading} height={"30px"}>
+            <HStack justifyContent={"flex-start"} w="100%">
+              <Text>
+                {data?.toilets} toilet{data?.toilets === 1 ? "" : "s"}
+              </Text>
+              <Text>∙</Text>
+              <Text>
+                {data?.rooms} room{data?.rooms === 1 ? "" : "s"}
+              </Text>
+            </HStack>
+          </Skeleton>
+        </VStack>
+        <Avatar name={data?.owner.name} size={"xl"} src={data?.owner.avatar} />
+      </HStack>
+      <Box mt={10}>
+        <Heading mb={5} fontSize={"2xl"}>
+          <HStack>
+            <FaStar />
+            <Text>{data?.rating}</Text>
+            <Text>∙</Text>
+            <Text>
+              {reviewsData?.length} review
+              {reviewsData?.length === 1 ? "" : "s"}
+            </Text>
+          </HStack>
+        </Heading>
+        <Container mt={16} maxW="container.lg" marginX="none">
+          <Grid gap={10} templateColumns={"1fr 1fr"}>
+            {reviewsData?.map((review, index) => (
+              <VStack alignItems={"flex-start"} key={index}>
+                <HStack>
+                  <Avatar
+                    name={review.user.name}
+                    src={review.user.avatar}
+                    size="md"
+                  />
+                  <VStack spacing={0} alignItems={"flex-start"}>
+                    <Heading fontSize={"md"}>{review.user.name}</Heading>
+                    <HStack spacing={1}>
+                      <FaStar size="12px" />
+                      <Text>{review.rating}</Text>
+                    </HStack>
+                  </VStack>
+                </HStack>
+                <Text>{review.payload}</Text>
+              </VStack>
+            ))}
+          </Grid>
+        </Container>
+      </Box>
     </Box>
   );
 }
